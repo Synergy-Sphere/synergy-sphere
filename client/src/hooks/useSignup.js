8d1ae967440c7b4fedb2e920ffbd381abd41004b
Ali Mekael <ali.mekael@dci-student.org>
2024-07-01 15:22:59 +0200

    signing up functionality added

---
+++ client/src/hooks/useSignup.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+import { useRegisterContext } from "../contexts/registerContext/RegisterContext";
+
+function useSignup() {
+  const [loading, setLoading] = useState(false);
+  const { signupInfo, registerDispatch, REGISTER_TYPES } = useRegisterContext();
+  // const { fullName, username, email, password, confirmPassword } = signupInfo;
+
+  async function signingUp({
+    fullName,
+    username,
+    email,
+    password,
+    confirmPassword,
+  }) {
+    const valid = checkInputsValidations({
+      fullName,
+      username,
+      email,
+      password,
+      confirmPassword,
+    });
+    if (!valid) return;
+    setLoading(true);
+
+    try {
+      const settings = {
+        method: "POST",
+        body: JSON.stringify({
+          fullName,
+          username,
+          email,
+          password,
+          confirmPassword,
+        }),
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      };
+      const response = await fetch("", settings);
+      if (response.ok) {
+        const data = await response.json();
+        registerDispatch({
+          type: REGISTER_TYPES.ASSIGN_LOGGED_IN_USER,
+          payload: data,
+        });
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { loading, signingUp };
+}
+
+export default useSignup;
+
+function checkInputsValidations({
+  fullName,
+  username,
+  email,
+  password,
+  confirmPassword,
+}) {
+  if (!fullName || !username || !email || !password || !confirmPassword) {
+    toast.error("No empty fields");
+    return false;
+  }
+  if (password !== confirmPassword) {
+    toast.error("Passwords don't match");
+    return false;
+  }
+  if (password.length < 6) {
+    toast.error("Password must be at least 6 characters");
+    return false;
+  }
+
+  return true;
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import RegisterContextProvider from "./contexts/registerContext/RegisterContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RegisterContextProvider>
+        <App />
+      </RegisterContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
