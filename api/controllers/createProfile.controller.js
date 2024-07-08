eba068aa1dc25a77a0f4e2332ac45cc2c19d9ec5
Vladyslav Bakumenko <vladyslav.bakumenko@dci-student.org>
2024-07-08 15:20:22 +0200

    updateInterests controller func added

--- api/controllers/createProfile.controller.js
+++ api/controllers/createProfile.controller.js
@@ -25,9 +25,12 @@ export const updateBio = async (req, res, next) => {
         runValidators: true,
       };
 
-      const updatedUser = await User.findByIdAndUpdate(id, {
-        $set: { bio: bio } },
-        options,
+      const updatedUser = await User.findByIdAndUpdate(
+        id,
+        {
+          $set: { bio: bio },
+        },
+        options
       );
 
       res.status(201).json({
@@ -52,53 +55,56 @@ export const updateBio = async (req, res, next) => {
 };
 
 export const updateProfilePicture = async (req, res, next) => {
-    const id = req.params.id;
-    const tokenUserId = req.user.id;
-    const { profilePic } = req.body;
-  
-    if (id !== tokenUserId) {
-      return next(createError(403, "Not authorized"));
-    }
-  
-    let foundUser;
-  
+  const id = req.params.id;
+  const tokenUserId = req.user.id;
+  const { profilePic } = req.body;
+
+  if (id !== tokenUserId) {
+    return next(createError(403, "Not authorized"));
+  }
+
+  let foundUser;
+
+  try {
+    foundUser = await User.findById(id);
+  } catch (error) {
+    return next(createError(500, "Server error"));
+  }
+
+  if (foundUser) {
     try {
-      foundUser = await User.findById(id);
+      const options = {
+        new: true,
+        runValidators: true,
+      };
+
+      const updatedUser = await User.findByIdAndUpdate(
+        id,
+        {
+          $set: { profilePic: profilePic },
+        },
+        options
+      );
+
+      res.status(201).json({
+        id: updatedUser._id,
+        username: updatedUser.username,
+        email: updatedUser.email,
+        fullName: updatedUser.fullName,
+        profilePic: updatedUser.profilePic,
+        location: updatedUser.location,
+        bio: updatedUser.bio,
+        friendList: updatedUser.friendList,
+        interests: updatedUser.interests,
+        posts: updatedUser.posts,
+        events: updatedUser.events,
+      });
     } catch (error) {
       return next(createError(500, "Server error"));
     }
-  
-    if (foundUser) {
-      try {
-        const options = {
-          new: true,
-          runValidators: true,
-        };
-  
-        const updatedUser = await User.findByIdAndUpdate(id, {
-          $set: { profilePic: profilePic } },
-          options,
-        );
-  
-        res.status(201).json({
-          id: updatedUser._id,
-          username: updatedUser.username,
-          email: updatedUser.email,
-          fullName: updatedUser.fullName,
-          profilePic: updatedUser.profilePic,
-          location: updatedUser.location,
-          bio: updatedUser.bio,
-          friendList: updatedUser.friendList,
-          interests: updatedUser.interests,
-          posts: updatedUser.posts,
-          events: updatedUser.events,
-        });
-      } catch (error) {
-        return next(createError(500, "Server error"));
-      }
-    } else {
-      next(createError(404, "User not found"));
-    }
+  } else {
+    next(createError(404, "User not found"));
+  }
 };
 
 // export const updateCoverPicture = (req, res, next) => {
@@ -107,50 +113,106 @@ export const updateProfilePicture = async (req, res, next) => {
 
 export const updateLocation = async (req, res, next) => {
   const id = req.params.id;
-    const tokenUserId = req.user.id;
-    const { location } = req.body;
-  
-    if (id !== tokenUserId) {
-      return next(createError(403, "Not authorized"));
-    }
-  
-    let foundUser;
-  
+  const tokenUserId = req.user.id;
+  const { location } = req.body;
+
+  if (id !== tokenUserId) {
+    return next(createError(403, "Not authorized"));
+  }
+
+  let foundUser;
+
+  try {
+    foundUser = await User.findById(id);
+  } catch (error) {
+    return next(createError(500, "Server error"));
+  }
+
+  if (foundUser) {
     try {
-      foundUser = await User.findById(id);
+      const options = {
+        new: true,
+        runValidators: true,
+      };
+
+      const updatedUser = await User.findByIdAndUpdate(
+        id,
+        {
+          $set: { location: location },
+        },
+        options
+      );
+
+      res.status(201).json({
+        id: updatedUser._id,
+        username: updatedUser.username,
+        email: updatedUser.email,
+        fullName: updatedUser.fullName,
+        profilePic: updatedUser.profilePic,
+        location: updatedUser.location,
+        bio: updatedUser.bio,
+        friendList: updatedUser.friendList,
+        interests: updatedUser.interests,
+        posts: updatedUser.posts,
+        events: updatedUser.events,
+      });
     } catch (error) {
       return next(createError(500, "Server error"));
     }
-  
-    if (foundUser) {
-      try {
-        const options = {
-          new: true,
-          runValidators: true,
-        };
-  
-        const updatedUser = await User.findByIdAndUpdate(id, {
-          $set: { location: location } },
-          options,
-        );
-  
-        res.status(201).json({
-          id: updatedUser._id,
-          username: updatedUser.username,
-          email: updatedUser.email,
-          fullName: updatedUser.fullName,
-          profilePic: updatedUser.profilePic,
-          location: updatedUser.location,
-          bio: updatedUser.bio,
-          friendList: updatedUser.friendList,
-          interests: updatedUser.interests,
-          posts: updatedUser.posts,
-          events: updatedUser.events,
-        });
-      } catch (error) {
-        return next(createError(500, "Server error"));
-      }
-    } else {
-      next(createError(404, "User not found"));
+  } else {
+    next(createError(404, "User not found"));
+  }
+};
+
+export const updateInterests = async (req, res, next) => {
+  const id = req.params.id;
+  const tokenUserId = req.user.id;
+  const { interests } = req.body;
+
+  if (id !== tokenUserId) {
+    return next(createError(403, "Not authorized"));
+  }
+
+  let foundUser;
+
+  try {
+    foundUser = await User.findById(id);
+  } catch (error) {
+    return next(createError(500, "Server error"));
+  }
+
+  if (foundUser) {
+    try {
+      const options = {
+        new: true,
+        runValidators: true,
+      };
+
+      const updatedUser = await User.findByIdAndUpdate(
+        id,
+        {
+          $set: { interests: interests },
+        },
+        options
+      );
+
+      res.status(201).json({
+        id: updatedUser._id,
+        username: updatedUser.username,
+        email: updatedUser.email,
+        fullName: updatedUser.fullName,
+        profilePic: updatedUser.profilePic,
+        location: updatedUser.location,
+        bio: updatedUser.bio,
+        friendList: updatedUser.friendList,
+        interests: updatedUser.interests,
+        posts: updatedUser.posts,
+        events: updatedUser.events,
+      });
+    } catch (error) {
+      return next(createError(500, "Server error"));
     }
+  } else {
+    next(createError(404, "User not found"));
+  }
 };
--- api/routes/createProfile.route.js
+++ api/routes/createProfile.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {updateProfilePicture,updateBio,updateLocation } from "../controllers/createProfile.controller.js";
+import {updateProfilePicture,updateBio,updateLocation, updateInterests } from "../controllers/createProfile.controller.js";
 import authenticateToken from "../middleware/authenticateToken.js";
 
 const router = Router();
@@ -8,6 +8,7 @@ router.patch("/:id/profilePicture", authenticateToken, updateProfilePicture);
 router.patch("/:id/bio", authenticateToken, updateBio);
 // router.patch("/:id/coverPicture", authenticateToken, updateCoverPicture);
 router.patch("/:id/location", authenticateToken, updateLocation);
+router.patch("/:id/interests", authenticateToken, updateInterests);
 
 
 export default router;
